78a3560e32b744e19d111753212dc1cd69b5c685
fangz2 <fangz2@rose-hulman.edu>
2017-11-27 15:52:54 -0500

    done

--- src/m5_your_turtles.py
+++ src/m5_your_turtles.py
@@ -33,41 +33,43 @@ import rosegraphics as rg
 window = rg.TurtleWindow()
 roof= rg.SimpleTurtle()
 roof.pen = rg.Pen('midnight blue', 3)
-roof.speed=1114
-roof.forward(100)
-roof.left(120)
-roof.forward(100)
-roof.left(120)
-roof.forward(100)
-roof.left(30)
-roof.forward(100)
-roof.left(90)
-roof.forward(40)
-roof.draw_regular_polygon(4,20)
-roof.forward(60)
-roof.left(90)
-roof.forward(100)
+roof.speed=1114000000
 
+for k in range(12):
+    roof.forward(100)
+    roof.left(120)
+    roof.forward(100)
+    roof.left(120)
+    roof.forward(100)
+    roof.left(30)
+    roof.forward(100)
+    roof.left(90)
+    roof.forward(40)
+    roof.draw_regular_polygon(4,20)
+    roof.forward(60)
+    roof.left(90)
+    roof.forward(100)
+    roof.right(60)
 
+window = rg.TurtleWindow()
+qwe= rg.SimpleTurtle()
+qwe.pen = rg.Pen('red', 3)
+qwe.speed=1114
 
+for k in range(12):
+    qwe.pen_up()
+    qwe.forward(100)
+    qwe.pen_down()
 
+    qwe.left(90)
+    qwe.forward(100)
+    qwe.left(90)
+    qwe.forward(20)
+    qwe.left(90)
+    qwe.forward(100)
+    qwe.left(90)
+    qwe.forward(20)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    qwe.left(30)
 
 window.close_on_mouse_click()
